0853713be76bf2f8e2037f25970ccc3ae4941b68
David Stålgren <115017669+davidstalgren@users.noreply.github.com>
2023-03-23 09:38:01 +0100

    create order endpoint now update stock for product

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -9,12 +9,34 @@ router.post('/add', async(req, res, next) => {
   // SKAPA ORDER FÖR EN SPECIFIK USER // PRODUCTS ÄR EN ARRAY MOTSVARANDE INNEHÅLLET I KUNDVAGN
 
   try {
+    const { products } = req.body;
+
+    for (const item of products) {
+      const product = await ProductModel.findById(item.productId);
+      console.log(product);
+
+      if (product.lager < item.quantity) {
+        console.log('insufficient stock');
+      };
+
+      product.lager -= item.quantity;
+      await product.save();
+    };
+    const order = new OrderModel(req.body);
+    await order.save();
+
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: 'Something went wrong' });
+  }
+
+  /* try {
     const order = await OrderModel.create(req.body);
     res.status(201).json(order)
   } catch (error) {
     console.log(error);
     res.status(400).json({ error: 'Something went wrong' });
-  }
+  } */
 });
 
 
@@ -29,14 +51,6 @@ router.get('/all', async (req, res, next) => {
     console.log(error);
     res.status(400).json({ error: 'Something went wrong' });
   }
-
-/*   try {
-    const orders = await OrderModel.find();
-    res.status(200).json(orders)
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ error: 'Something went wrong' });
-  } */
 });
 
 
